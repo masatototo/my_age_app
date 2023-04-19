61ee8b702edf51a523334ac2d6c0cb06ca855d58
masatototo <m.sasaki3891@gmail.com>
2023-04-19 17:27:51 +0900

    ogp-png

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export const Home = () => {
       <meta property="og:description" content="顔診断" />
       <meta property="og:url" content="https://my-age-app.vercel.app/" />
       <meta property="og:site_name" content="MyAge 顔診断" />
-      <meta key="og:image" property="og:image" content="https://vercel.com/f9582bfb-d169-46dc-baf6-b7b8f29c3709"/>
+      <meta key="og:image" property="og:image" content="https://my-age-app.vercel.app/images/ogp.png"/>
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@sasakimasato9" />
     </Head>
