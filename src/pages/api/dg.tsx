c8da72600d5e94d8f4a28508bb822e3db8d61f68
masatototo <m.sasaki3891@gmail.com>
2023-04-19 16:39:19 +0900

    b

--- src/pages/api/dg.tsx
+++ src/pages/api/dg.tsx
@@ -6,19 +6,17 @@ export const config = {
   runtime: 'experimental-edge',
 }
 
+
 export default async function handler() {
-  const [robotoData, mplusData] = await Promise.all([
-    fetch(
-      'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxK.woff2'
-    ).then((res) => res.arrayBuffer()),
-    fetch(
-      'https://fonts.gstatic.com/s/mplus1p/v14/e3tleu03ZqcP9PbSXDGHvAzyDMXhdD8sAj6OAJTFsNM.woff2'
-    ).then((res) => res.arrayBuffer()),
-  ]);
+  const fontData = await fetch(
+    new URL('../../assets/Roboto-Bold.ttf', import.meta.url)
+  ).then((res) => res.arrayBuffer())
 
   return new ImageResponse(
     (
+      
       <div
+      
         style={{
           display: 'flex',
           justifyContent: 'center',
@@ -27,8 +25,7 @@ export default async function handler() {
           height: '100%',
           backgroundColor: '#869ef5',
           fontSize: '128px',
-          fontFamily: 'M PLUS 1p, Roboto, sans-serif',
-          fontWeight: 700,
+          fontFamily: '"Roboto"',
         }}
       >
         MyAge 顔診断
@@ -37,55 +34,12 @@ export default async function handler() {
     {
       fonts: [
         {
-          name: 'M PLUS 1p',
-          data: mplusData,
-          weight: 700,
-          style: 'normal',
-        },
-        {
           name: 'Roboto',
-          data: robotoData,
+          data: fontData,
           weight: 700,
           style: 'normal',
         },
       ],
     }
-  );
+  )
 }
-
-// export default async function handler() {
-//   const fontData = await fetch(
-//     new URL('../../assets/Roboto-Bold.ttf', import.meta.url)
-//   ).then((res) => res.arrayBuffer())
-
-//   return new ImageResponse(
-//     (
-      
-//       <div
-      
-//         style={{
-//           display: 'flex',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//           width: '100%',
-//           height: '100%',
-//           backgroundColor: '#869ef5',
-//           fontSize: '128px',
-//           fontFamily: '"Roboto"',
-//         }}
-//       >
-//         MyAge 顔診断
-//       </div>
-//     ),
-//     {
-//       fonts: [
-//         {
-//           name: 'Roboto',
-//           data: fontData,
-//           weight: 700,
-//           style: 'normal',
-//         },
-//       ],
-//     }
-//   )
-// }
