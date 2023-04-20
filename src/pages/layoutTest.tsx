11075c8cb47cbd11f7b234ab578174761300b355
masatototo <m.sasaki3891@gmail.com>
2023-04-20 14:39:11 +0900

    fix webcam

--- src/pages/layoutTest.tsx
+++ src/pages/layoutTest.tsx
@@ -1,11 +1,27 @@
 import styles from '../styles/layout.module.css'
+import Webcam from "react-webcam";
+import { useRef } from 'react';
+import { isMobile } from 'react-device-detect';
+
 
 export const HomePage = () => {
+    const webcamRef = useRef<Webcam>(null);
+
     return (
         <>
-            <div className={styles.div1}>1</div>
-            <div className={styles.div2}>2</div>
-            <div className={styles.div3}>3</div>
+            <div className={styles.div1}></div>
+            <div className={styles.div2}>
+                <Webcam
+                audio={false}
+                width={'100%'}
+                // height={'100%'}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                screenshotQuality={1}
+                videoConstraints={(isMobile) ? {facingMode:{exact:"environment"}} : {facingMode:"user"}}
+                />
+            </div>
+            <div className={styles.div3}></div>
         </>
     )
 }
